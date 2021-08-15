49eb9a65b5a188c52f25e42ac00d45a7faa3aa2e
duyng2512 <duyng2512@gmail.com>
2021-08-15 19:30:45 +0700

    init project

---
+++ src/main/java/com/dinj/docclinic/DocClinicApplication.java
@@ -0,0 +1,13 @@
+package com.dinj.docclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocClinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DocClinicApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dinj/docclinic/DocClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dinj.docclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DocClinicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
